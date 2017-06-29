4f96a762f754b784d240b5ce195987e750badf23
Topper Bowers <topper@toppingdesign.com>
2017-06-29 10:14:53 +0200

    relative path for index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-exports.Device = require('src/device');
-exports.Identity = require('src/identity');
-exports.Ownership = require('src/ownership');
-exports.qcpb = require('src/qc_pb_with_extra');
-exports.Simpcert = require('src/simpcert');
+exports.Device = require('./src/device');
+exports.Identity = require('./src/identity');
+exports.Ownership = require('./src/ownership');
+exports.qcpb = require('./src/qc_pb_with_extra');
+exports.Simpcert = require('./src/simpcert');
