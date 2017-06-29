f928138bb8f32996d3caee85321057e5a114d05d
Topper Bowers <topper@toppingdesign.com>
2017-06-29 11:15:13 +0200

    add a crypto shim for envs that don't support node

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+try {
+   require.resolve("crypto");
+} catch(e) {
+   require('app-module-path').addPath("./src/shims");
+}
+
 exports.Device = require('./src/device');
 exports.Identity = require('./src/identity');
 exports.Ownership = require('./src/ownership');
