b4d1aedbd86691d902fc25d87b34d6519804b1d3
Topper Bowers <topper@toppingdesign.com>
2017-07-06 11:27:23 +0200

    export the internal forge module as well.

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ exports.Ownership = require('./src/ownership');
 exports.qcpb = require('./src/qc_pb_with_extra');
 exports.Simpcert = require('./src/simpcert');
 exports.SignableRequest = require('./src/signable_request');
+exports.forge = require('./src/forge');
